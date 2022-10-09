c0eee4eafd83a0df66f72d8a9104f35ca47442fa
AndyNguyen002 <ngocdungokla@gmail.com>
2022-10-09 10:29:32 +0700

    update

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import * as React from "react";
-import { Button, View, Text, TextInput, StyleSheet } from "react-native";
+import {
+  Button,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  ImageBackground,
+  Alert,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Buttonn } from "./Components/Buttonn";
+import { useState } from "react";
+
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate("Login")}
+        color="lightcoral"
+      />
       <Text></Text>
       <Button
         title="Register"
@@ -21,11 +36,40 @@ function HomeScreen({ navigation }) {
     </View>
   );
 }
+function Login({ navigation }) {
+  const [Account, setAccount] = useState("");
+  const [Password, setPassword] = useState("");
+  const handleClick = () => {
+    if (Account && Password) {
+      return navigation.navigate("HomePage");
+    } else {
+      Alert.alert("Enter username and password");
+    }
+  };
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <TextInput
+        onChangeText={setAccount}
+        placeholder="UserName"
+        style={styles.input}
+        value={Account}
+      />
+      <TextInput
+        onChangeText={setPassword}
+        placeholder="PassWord"
+        style={styles.input}
+        value={Password}
+      />
+      <Buttonn callback={handleClick}>Login</Buttonn>
+    </View>
+  );
+}
 function Register() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <TextInput placeholder="UserName" style={styles.input} />
       <TextInput placeholder="PassWord" style={styles.input} />
+      <TextInput placeholder="Re enter password" style={styles.input} />
       <Buttonn>Register</Buttonn>
     </View>
   );
@@ -38,6 +82,14 @@ function ForgotPassword() {
     </View>
   );
 }
+function AfterLogin() {
+  return (
+    <ImageBackground
+      source={require("./image/login.jpg")}
+      style={{ flex: 1, width: "100%", height: "100%" }}
+    ></ImageBackground>
+  );
+}
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -47,6 +99,8 @@ function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="HomePage" component={AfterLogin} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -63,6 +117,13 @@ const styles = StyleSheet.create({
   },
   btn: {
     width: "70%",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#dddddd",
+    flexDirection: "column",
   },
 });
 
